79ed715654f73571149c62b0a744d5d2ebe1e04b
Rehan7A <rehanansari6565@gmail.com>
2025-10-07 23:17:26 +0530

    fix(native): add method for ios

--- ios/DeviceGreeter.mm
+++ ios/DeviceGreeter.mm
@@ -7,6 +7,12 @@
     return result;
 }
 
+// ✅ New add method
+- (NSNumber *)add:(double)a b:(double)b {
+  NSNumber *result = @(a + b);
+  return result;
+}
+
 - (std::shared_ptr<facebook::react::TurboModule>)getTurboModule:
     (const facebook::react::ObjCTurboModule::InitParams &)params
 {
